ea163e9f3be87b5cc47fbad840c029a82b915314
Akash Dhangar <aakashdhangar2k10@gmail.com>
2025-05-18 18:01:33 +0530

    add code in databse file to connect database

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php 
+class database{
+    private $host;
+    private $dbusername;
+    private $dbpassword;
+    private $dbname;
+
+    protected function connect(){
+        $this->host='localhost';
+        $this->dbusername='root';
+        $this->dbpassword='';
+        $this->dbname='oops_curd_db';
+        $con= new mysqli($this->host,$this->dbusername,$this->dbpassword,$this->dbname);
+        return $con;
+    }
+}
+
+?>
