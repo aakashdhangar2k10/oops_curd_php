0f16ce8e43bdcecbb472bb797a441bb188add2a4
Akash Dhangar <aakashdhangar2k10@gmail.com>
2025-05-19 22:30:31 +0530

    add update function in database.php file

--- database.php
+++ database.php
@@ -79,6 +79,24 @@ public function insertData($table,$condition_arr){
 			$result=$this->connect()->query($sql);
 		}
 	}
+
+	public function updateData($table,$condition_arr,$where_field,$where_value){
+		if($condition_arr!=''){
+			$sql="update $table set ";
+			$c=count($condition_arr);	
+			$i=1;
+			foreach($condition_arr as $key=>$val){
+				if($i==$c){
+					$sql.="$key='$val'";
+				}else{
+					$sql.="$key='$val', ";
+				}
+				$i++;
+			}
+			$sql.=" where $where_field='$where_value' ";
+			$result=$this->connect()->query($sql);
+		}
+	}
 }
 
 ?>
